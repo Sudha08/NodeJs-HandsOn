f2518063a390f82d0dfcc983e4f71655a00e1b36
SUDHA CHANDRAMOULEESWARAN <963209@TCS.com>
2018-11-22 12:58:16 +0530

    Express + templating engines

--- expRoute.js
+++ expRoute.js
@@ -2,12 +2,49 @@ var express = require('express');
 
 var app = express(); // to access all the different methods available in express
 
-app.get('/', function(req, res) { // need not send the content type
-    res.send('this is the homepage');
+app.set('view engine', 'ejs'); // assign the template engine to Express
+app.use('/assets', express.static('assets'));
+
+// app.get('/', function(req, res) { 
+//     res.send('this is the homepage');
+// });
+
+app.get('/help', function(req, res) { // need not send the content type
+    res.send('this is the help page');
 });
 
-app.get('/contact', function(req, res) { // need not send the content type
-    res.send('this is the contact page');
+// app.get('/', function(req, res) { // to send a html page to the client
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/contact', function(req, res) {
+//     res.sendFile(__dirname + '/contact.html');
+// });
+
+app.get('/search/:id', function(req, res) { // eg:- Express route params
+    res.send('You requested to search the Id ' + req.params.id);
+});
+
+// using template engines - basic
+// app.get('/profile/:name', function(req, res) {
+//     // method will be mostly used to deal with dynamic data (like data from DB)
+//     var data = {age: 20, job: 'techie'};
+//     res.render('profile', {person: req.params.name, data: data}); 
+// });
+
+// using template engines - more data
+app.get('/profile/:name', function(req, res) {
+    // method will be mostly used to deal with dynamic data (like data from DB)
+    var data = {age: 20, job: 'techie', hobbies: ['eating', 'fighting', 'coding']};
+    res.render('profile', {person: req.params.name, data: data}); 
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
 });
 
 
